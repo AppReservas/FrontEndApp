8f2a15ae2b443a59c486b0324eea07ea76fa47b5
sebasco015 <sebasco015@gmail.com>
2023-09-13 21:10:01 -0500

    Arreglo gitignore y api

--- src/api/reservasApi.tsx
+++ src/api/reservasApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const baseURL = 'http://192.168.50.70:8080/api'
+const baseURL = 'http://192.168.0.14:8080/api'
 
 const reservasApi = axios.create({baseURL});
 
